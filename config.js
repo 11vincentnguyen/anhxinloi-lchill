cce8e2280fbece797a776da9d3c56b0a2751c95b
Vincent Nguyen <98414710+11vincentnguyen@users.noreply.github.com>
2022-01-26 03:41:11 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '✨^^Hihi^^ ✨',
+    introDesc: `Đố em biết bên trong có gì...`,
+    btnIntro: '^^Hihi^^',
+    title: 'Ichill Monday 01i Sunday ',
+    desc: 'Em là nhất 🤘 ',
+    btnYes: 'Tha lỗi cho anh <3',
+    btnNo: 'Còn lâu nhaaa',
+    question:'Anh làm cái này chỉ để xin lỗi em vì đã để em phải chờ anh.',
+    reasonPlaceholder: 'Em gõ gì đi...',
+    btnReply: 'Gửi cho anh 💌',
+    reply: 'Em tha lỗi cho anh đó ahihi',
+    mess: 'Anh biết mà lần sau sẽ không thế nữa. Thật đấy!',
+    messDesc: 'Inbox cho Anh nha. 💬',
+    btnAccept: 'Ok lunnnn',
+    messLink: 'https://m.me/11.vincent.nguyen'
+}
